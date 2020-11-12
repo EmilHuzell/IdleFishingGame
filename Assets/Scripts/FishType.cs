ce3c6af9d442a9864ab214608e4e6b708c67cd0c
XiaodiW <xiaodi.wang@student.forsbergsskola.se>
2020-11-12 10:20:08 +0100

    Adds Fishtype scriptObject

---
+++ Assets/Scripts/FishType.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FishType : ScriptableObject {
+    public int sizeMin = 5;
+    public int sizeMax = 10;
+    public float goldMultifier = 2;
+}
+

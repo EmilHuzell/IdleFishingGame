63752986fe95e5faa6e30ad1ba2ae5834b377888
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-11-12 09:14:06 +0100

    Added Gold Class

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+    public int CurrentGold => PlayerPrefs.GetInt("Gold");    
+    
+    public void AddGold(float goldToAdd) {
+        PlayerPrefs.SetInt("Gold", CurrentGold + Mathf.RoundToInt(goldToAdd));
+    }
+
+    public bool RemoveGold(float goldToRemove) {
+        if (goldToRemove >= 0f) {
+            PlayerPrefs.SetInt("Gold", CurrentGold - Mathf.RoundToInt(goldToRemove));
+            return true;
+        }else return false;
+    }
+}

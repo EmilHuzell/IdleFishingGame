3b962401d60c1ef0fe0715f285bc64f4b74b8090
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-11-12 12:04:08 +0100

    adds AutoFisher & FScene
    Autofisher can be added to a gameobject and get assigned a fisherType.
    
    FScene is Felix's test scene, To avoid conflicts.

--- Assets/Scripts/AutoFisherType.cs
+++ Assets/Scripts/AutoFisherType.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutoFisherType : MonoBehaviour
+[CreateAssetMenu(menuName = "Types/AutoFisherType")]
+public class AutoFisherType : ScriptableObject
 {
     public Texture texture;
-    public int BaseProduce;
-    public float ProduceIncrease;
-    public int BaseCost;
-    public float CostIncrease;
+    public int BaseProduce = 10;
+    //public float ProduceIncrease = 1.05f;
+    public int BaseCost = 10;
+    public float CostIncrease = 1.05f;
+    public float ProduceTime = 1;
 
-    public int CurrentCost(int upgradeAmount)
+    public int CurrentCost(int producerAmount)
     {
-        return Mathf.RoundToInt(BaseCost * Mathf.Pow(CostIncrease, upgradeAmount));
+        return Mathf.RoundToInt(BaseCost * Mathf.Pow(CostIncrease, producerAmount));
     }
     public int CurrentProduction(int producerAmount)
     {
-        return Mathf.RoundToInt(BaseProduce * Mathf.Pow(ProduceIncrease, producerAmount));
+        return Mathf.RoundToInt(BaseProduce * producerAmount);
     }
 }

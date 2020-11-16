1a0b80c753f08f473f91380767a018f1c75ee71b
XiaodiW <xiaodi.wang@student.forsbergsskola.se>
2020-11-16 13:06:24 +0100

    Correct Errors in FishSetup

--- Assets/Scripts/Fish/FishSetup.cs
+++ Assets/Scripts/Fish/FishSetup.cs
@@ -6,19 +6,21 @@ public class FishSetup : MonoBehaviour
     public FishUI prefab;
 
     public string currentTool = "spear";
-
-
-
-    public void getFish(this.currentTool)
+    
+    public void getFish(string tool)
     {
         int maxSize;
-        switch(tool){
+        switch (tool)
+        {
             case "spear":
-              maxSize = 1;
-              break;
+                maxSize = 1;
+                break;
             case "rod":
-              maxSize = 2;
-              break;
+                maxSize = 2;
+                break;
+            default:
+                maxSize = 0;
+                break;
         }
         var fish = Fishes[Random.Range(0, maxSize)];
         var newFish = Instantiate(prefab, transform);

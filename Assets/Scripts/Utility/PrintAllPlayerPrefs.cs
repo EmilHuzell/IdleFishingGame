c4f4a96627ca656584293232d85a808e989a1661
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-11-17 10:16:50 +0100

    Added custom tool to print all playerprefs

---
+++ Assets/Scripts/Utility/PrintAllPlayerPrefs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.EditorTools;
+
+[EditorTool("Log all playerprefs")]
+class PrintAllPlayerPrefs : EditorTool
+{
+    public override void OnToolGUI(EditorWindow window) {
+        
+        Debug.Log($"Current name: {PlayerPrefs.GetString("Name")}");
+        Debug.Log($"Current gender: {PlayerPrefs.GetString("Gender")}");
+        Debug.Log($"Current gold: {PlayerPrefs.GetString("Gold")}");
+        Debug.Log($"Current fisherkidamount: {PlayerPrefs.GetString("FisherKid_Amount")}");
+        Debug.Log($"Current fisherkidcost: {PlayerPrefs.GetString("FisherKid_Cost")}");
+        Debug.Log($"Current savedtime: {PlayerPrefs.GetString("savedTime")}");
+        
+        DestroyImmediate(this);
+    }
+}

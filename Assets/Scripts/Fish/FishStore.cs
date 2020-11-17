de2e1d8cfeff82ad9275c48f36740dba0159fdb0
XiaodiW <xiaodi.wang@student.forsbergsskola.se>
2020-11-17 11:07:10 +0100

    Arrange FishStore UI

--- Assets/Scripts/Fish/FishStore.cs
+++ Assets/Scripts/Fish/FishStore.cs
@@ -7,7 +7,7 @@ namespace Fish
 {
     public class FishStore : MonoBehaviour
     {
-        public FishType fish;
+        private FishType fish;
         public RawImage fishImage;
         public Text fishWeight;
         public Text button1;
@@ -19,12 +19,18 @@ namespace Fish
         public float updateInterval = 5;
         private float elapsedTime;
 
+        private void Start()
+        {
+            priceUI();
+        }
+
         private void Update()
         {
             fishWeight.text = $"{fish.Weight}";
             button1.text = $"X1";
             button2.text = $"X10";
             button3.text = $"100";
+            UpdatePrice();
         }
         
         private void UpdatePrice()
@@ -32,10 +38,15 @@ namespace Fish
             elapsedTime += Time.deltaTime;
             if (elapsedTime >= updateInterval)
             {
-                marketPrice = Random.Range(fish.minPrice,fish.minPrice*maxPriceMultifier);
-                marketText.text = $"{marketPrice} Gold/Kg";
+                priceUI();
                 elapsedTime = 0;
             }
+        }
+
+        private void priceUI()
+        {
+            marketPrice = Random.Range(fish.minPrice,fish.minPrice*maxPriceMultifier);
+            marketText.text = $"{marketPrice.ToString("F")} Gold/Kg";
         }
 
         public void setup(FishType fishType)

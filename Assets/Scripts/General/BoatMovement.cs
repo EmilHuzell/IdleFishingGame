e2f22cbb8618db8200a820026429f9fec0770c84
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-11-17 12:16:39 +0100

    Added Boat Prefab with random Boat and bob functions

---
+++ Assets/Scripts/General/BoatMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+public class BoatMovement : MonoBehaviour {
+    
+    public List<Sprite> boatImages;
+    public float movementSpeed;
+    public float bobAmount;
+    public float bobSpeed;
+    
+    private float _timePassed;
+    private float _x;
+    private float _y;
+
+    private RectTransform _transform;
+
+    private void Awake() {
+        _transform = GetComponent<RectTransform>();
+        var getRandomBoatSprite = boatImages[Random.Range(0, boatImages.Count())];
+        this.GetComponent<Image>().sprite = getRandomBoatSprite;
+    }
+
+    private void FixedUpdate() {
+        _x += 0.001f * movementSpeed;
+        _y = Mathf.Lerp(-bobAmount * 0.2f, bobAmount * 0.2f, Mathf.PingPong(Time.time * bobSpeed * 0.4f, 1));
+        
+        transform.localPosition += new Vector3(_x, _y);
+    }
+}

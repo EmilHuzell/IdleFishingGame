05ec7149a155d02149c0e62354a04756b4d9d3cf
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-11-17 13:23:48 +0100

    adds Ascension

---
+++ Assets/Scripts/General/Ascension.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Ascension
+{
+    public static int Amount { get => PlayerPrefs.GetInt("Ascension_Amount", 0); set => PlayerPrefs.SetInt("Ascension_Amount", value); }
+
+    public static float Multiplier { get => (Amount * 1.1f) + 1; }
+
+    public static void Ascend()
+    {
+        foreach (IAscend item in Object.FindObjectsOfType<Component>())
+        {
+            item.Ascend();
+        }
+    }
+}
+public interface IAscend
+{
+    void Ascend();
+}

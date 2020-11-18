a9bd92b05c9e4314fdbc8c916529211323811c38
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-11-18 21:38:28 +0100

    fixes some UI

--- Assets/Scripts/General/Ascension.cs
+++ Assets/Scripts/General/Ascension.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Numerics;
 
 public static class Ascension
 {
     public static int Amount { get => PlayerPrefs.GetInt("Ascension_Amount", 0); set => PlayerPrefs.SetInt("Ascension_Amount", value); }
 
     public static float Multiplier { get => (Amount * 1.1f) + 1; }
+
+    public static BigInteger Requirement { get =>  BigInteger.Pow(10, PlayerPrefs.GetInt("Ascension_Amount", 0)); }
 
     public static void Ascend()
     {

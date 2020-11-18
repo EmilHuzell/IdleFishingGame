9c227b1cd0d39090c3fd24bf0800fb1eddcb2761
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-11-18 15:56:49 +0100

    Changed boat to be more modular on-fly

--- Assets/Scripts/General/BoatSpawner.cs
+++ Assets/Scripts/General/BoatSpawner.cs
@@ -1,11 +1,28 @@
-﻿using Unity.Mathematics;
+﻿using System;
+using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class BoatSpawner : MonoBehaviour {
     public float intervalBetweenSpawns;
     public GameObject boatPrefab;
+    public float netBoatSize;
+    public float rodBoatSize;
+    public float harpoonBoatSize;
+
+    public float netBoatYLayer2, netBoatYLayer3, netBoatYLayer4;
+    public float rodBoatYLayer2, rodBoatYLayer3, rodBoatYLayer4;
+    public float harpoonBoatYLayer2, harpoonBoatYLayer3, harpoonBoatYLayer4;
+
     private float _checker = 0f;
+
+    private void Start() {
+        GameObject go = Instantiate(boatPrefab, this.transform);
+        var zVariation = Random.Range(2, 4);
+        var _y = CheckYPosition(zVariation, go);
+        go.transform.position = new Vector3(-Camera.main.rect.width - 15, _y, zVariation);
+    }
+
     private void FixedUpdate() {
         _checker += Time.deltaTime;
 
@@ -20,42 +37,45 @@ public class BoatSpawner : MonoBehaviour {
 
     private float CheckYPosition(int zVariation, GameObject go) {
         if (go.GetComponent<SpriteRenderer>().sprite.name == "Idle_Producer_Net_boat") {
+            go.transform.localScale = new Vector3(netBoatSize, netBoatSize);
             if (zVariation == 2) {
-                return 0f;
+                return netBoatYLayer2;
             }
 
             if (zVariation == 3) {
-                return 1.5f;
+                return netBoatYLayer3;
             }
 
             if (zVariation == 4) {
-                return 1.7f;
+                return netBoatYLayer4;
             }
         }
         else if (go.GetComponent<SpriteRenderer>().sprite.name == "Idle_Producer_Rod_boat") {
+            go.transform.localScale = new Vector3(rodBoatSize, rodBoatSize);
             if (zVariation == 2) {
-                return -1.2f;
+                return rodBoatYLayer2;
             }
 
             if (zVariation == 3) {
-                return 0.2f;
+                return rodBoatYLayer3;
             }
 
             if (zVariation == 4) {
-                return 0.4f;
+                return rodBoatYLayer4;
             }
         }
         else if (go.GetComponent<SpriteRenderer>().sprite.name == "Idle_Producer_spear_boat") {
+            go.transform.localScale = new Vector3(harpoonBoatSize, harpoonBoatSize);
             if (zVariation == 2) {
-                return -0.65f;
+                return harpoonBoatYLayer2;
             }
 
             if (zVariation == 3) {
-                return 2.4f;
+                return harpoonBoatYLayer3;
             }
 
             if (zVariation == 4) {
-                return 2.4f;
+                return harpoonBoatYLayer4;
             }
         }
         return -1000f;

cd80ccd7f2697d1ee0b4b4e8d8b2e695e4eaafb8
XiaodiW <xiaodi.wang@student.forsbergsskola.se>
2020-11-18 15:37:56 +0100

    Seperate Fish icon+Amount & FishMarket

---
+++ Assets/Scripts/Fish/FishStore.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fish
+{
+    public class FishStore : MonoBehaviour, IAscend
+    {
+        private FishType fish;
+        public RawImage fishImage;
+        public Text fishWeight;
+      
+        private void Update()
+        {
+            fishWeight.text = PrettifyText.Format(fish.Weight);
+        }
+
+        public void Ascend()
+        {
+            fish.Reset();
+        }
+
+        public void setup(FishType fishType)
+        {
+            gameObject.name = fishType.name;
+            fish = fishType;
+            fishImage.texture = fishType.image;
+        }
+    }
+}

dcfb612b82cb32590281b3a09e4e86ac2d1b7280
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-11-18 14:38:10 +0100

    combines Scenes through scripts

---
+++ Assets/Scripts/General/AddScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AddScene : MonoBehaviour
+{
+    public int sceneNumber;
+    public void Awake()
+    {
+        SceneManager.LoadScene(sceneNumber, LoadSceneMode.Additive);
+        Destroy(gameObject);
+    }
+}

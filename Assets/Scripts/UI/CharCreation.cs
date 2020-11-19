8b9dd1f6b78f85a5498f649e90409ae74a5d74a4
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-11-19 10:00:01 +0100

    Added character creation screen

--- Assets/Scripts/UI/CharCreation.cs
+++ Assets/Scripts/UI/CharCreation.cs
@@ -1,27 +1,7 @@
-﻿using System;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using Object = UnityEngine.Object;
 
 public class CharCreation : MonoBehaviour {
-
-    public InputField nameInput;
-    public GameObject nameStage;
-    public GameObject genderStage;
-    
-
-    private void Start() {
-        genderStage.gameObject.SetActive(false);
-    }
-
-    public void SubmitName() {
-        PlayerPrefs.SetString("Name", nameInput.text);
-        Debug.Log(PlayerPrefs.GetString("Name"));
-        nameStage.gameObject.SetActive(false);
-        genderStage.gameObject.SetActive(true);
-    }
     
     public void Male() {
         PlayerPrefs.SetString("Gender", "Male");

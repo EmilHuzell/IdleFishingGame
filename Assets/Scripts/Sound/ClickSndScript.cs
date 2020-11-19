4776568f0aa4aafd57a5106f80d3c6c7d53fd232
Bulteze <48334398+Bulteze@users.noreply.github.com>
2020-11-19 11:12:30 +0100

    Made pitch variation on sound for click button

---
+++ Assets/Scripts/Sound/ClickSndScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ClickSndScript : MonoBehaviour {
+    public float pitchMin, pitchMax; 
+        
+    public void PitchVariation() {
+        var pitch = Random.Range(pitchMin, pitchMax);
+        GetComponent<AudioSource>().pitch = pitch;
+    }
+}
